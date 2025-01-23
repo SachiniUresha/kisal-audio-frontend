530407348a507b6f798b45d6d6b1a041fdc4bfd8
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-01-23 21:07:00 +0530

    install Tailwindcss 😊

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import ProductCard from './components/productCard'
 function App() {
 
   return (
-    <>
-      <ProductCard name="audio setup" price="2000" description="jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-      " photourl="https://s.alicdn.com/@sc04/kf/H1051dcf177ba4d0da898e6078e1a04aat.jpg_720x720q50.jpg" />
-
-      <ProductCard name="flower" price="4000" descripion="nice flower" photourl="https://th.bing.com/th/id/OIP.z6pGwFhPvV4_Yn2ruRtQTwHaEK?rs=1&pid=ImgDetMain"  />
-    </>
+    <div>
+      <div></div>
+      
+    </div>
   )
 }
 
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -2,15 +2,11 @@ import "./productCard.css";
 
 export default function ProductCard(props){
 
-    console.log(props.price)
 
     return(
 
         <div>
-            <img src={props.photourl} />
-            <span>{props.name}</span>
-            <span>LKR. {props.price}</span>
-            <p>{props.description}</p>
+           
             
         </div>
     )

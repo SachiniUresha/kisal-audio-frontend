06355461360200d46458f69bedcedfa9e7bb28ec
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-04-17 14:55:41 +0530

    Create Admin Users Page ✔️

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -5,6 +5,7 @@ import {Link, Routes, Route} from 'react-router-dom';
 import AdminItemsPage from "./adminItemsPage";
 import AddItemPage from "./addItemPage";
 import UpdateItemPage from "./updateItemPage";
+import AdminUsersPage from "./adminUsersPage";
 
 
 
@@ -29,15 +30,16 @@ export default function AdminPage(){
             <MdOutlineSpeaker/>
           Items
           </Link>
-          <button className='w-full h-[40px] text-[25px] font-bold    flex justify-center items-center   '>
+          <Link className='w-full h-[40px] text-[25px] font-bold    flex justify-center items-center   ' to='/admin/users'>
           <FaRegUser/>
             Users
-          </button>
+          </Link>
   
         </div>
         <div className="w-[calc(100vw-200px)]">
           <Routes path="/*">
             <Route path="/bookings" element={<h1>Bookings</h1>}/>
+            <Route path="/users" element={<AdminUsersPage/>}/>
             <Route path="/items" element={<AdminItemsPage/>}/>
             <Route path="/items/add" element={<AddItemPage/>}/>
             <Route path="/items/edit" element={<UpdateItemPage/>}/>

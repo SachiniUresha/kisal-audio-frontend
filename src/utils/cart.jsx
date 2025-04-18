6afcb426f1a8b5fa7a66b02354f5c5177624b995
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-04-18 13:10:40 +0530

    Create Booking Function

--- src/utils/cart.jsx
+++ src/utils/cart.jsx
@@ -46,7 +46,7 @@ export function removeFromCart(key){
     localStorage.setItem("cart", cartString);
 }
 
-function formatDate(date){
+export function formatDate(date){
 
     const year = date.getFullYear();
     const month = String(date.getMonth()+1).padStart(2,'0');
